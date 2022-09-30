0c64d2bdb5622eca5467ca15b3eb091a0f9f32d8
Daniel <danieltkalla@gmail.com>
2022-09-30 14:15:02 -0400

    phase 2 done

--- Poly_TreeNodes/lib/00_tree_node.rb
+++ Poly_TreeNodes/lib/00_tree_node.rb
@@ -10,10 +10,36 @@ class PolyTreeNode
     end
 
     def parent=(parent_node)
-        # debugger
+        if @parent != nil
+            if @parent.children.include?(self)
+                @parent.children.reject! { |ele| ele == self }
+            end
+        end
         @parent = parent_node
-        # debugger
         parent_node.children << self if parent_node
-        # debugger
+    end
+
+    def add_child(child_node)
+        child_node.parent=(self)
+    end
+
+    def remove_child(child_node)
+        raise "Not a child" if !@children.include?(child_node)
+        child_node.parent=nil
     end
 end
+
+
+n1 = PolyTreeNode.new("root1")
+n2 = PolyTreeNode.new("root2")
+n3 = PolyTreeNode.new("root3")
+
+# connect n3 to n1
+n3.parent = n1
+# connect n3 to n2
+n3.parent = n2
+
+# this should work
+raise "Bad parent=!" unless n3.parent == n2
+raise "Bad parent=!" unless n2.children == [n3]
+raise "Bad parent=!" unless n1.children == []
