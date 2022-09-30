baf7cfa93c2e808068b8eb7fd960f0127807341a
Daniel <danieltkalla@gmail.com>
2022-09-30 12:17:07 -0400

    first round

--- Poly_TreeNodes/lib/00_tree_node.rb
+++ Poly_TreeNodes/lib/00_tree_node.rb
@@ -1,3 +1,17 @@
+require "byebug"
+
 class PolyTreeNode
-    
+    attr_reader :value, :parent, :children
+
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+    def parent=(parent_node)
+        debugger
+        parent = parent_node
+        self.children << parent_node
+    end
 end
