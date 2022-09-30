56f2d24ef2c40fd4f7fae680bab701454d46fa46
korycfitz <korycfitz@gmail.com>
2022-09-30 15:29:28 -0400

    finished bfs in phase 2

--- Knights_Travails/knight_path_finder.rb
+++ Knights_Travails/knight_path_finder.rb
@@ -1,9 +1,21 @@
 require_relative "poly_tree_node.rb"
 
 class KnightPathFinder
+    attr_reader :considered_positions
 
     def self.valid_moves(position)
-
+        row, col = position
+        changes = [[1,2],[1,-2],[-1,2],[-1,-2],[2,1],[2,-1],[-2,1],[-2,-1]]
+        new_moves = []
+        changes.each do |change|
+            row_change, col_change = change
+            new_row = row + row_change
+            new_col = col + col_change
+            if new_row >= 0 && new_row < 8
+                new_moves << [new_row, new_col] if new_col >= 0 && new_col < 8
+            end
+        end
+        new_moves
     end
 
     def initialize(starting_position = [0,0])
@@ -16,10 +28,18 @@ class KnightPathFinder
     end
 
     def new_move_positions(position)
-        moves = self.valid_moves(position)
+        moves = KnightPathFinder.valid_moves(position)
+        moves.reject! {|move| @considered_positions.include? move }
+        @considered_positions += moves
+        moves
     end
 
     # def find_path(ending_position)
     # end
 
 end
+
+test = KnightPathFinder.new([0,3])
+p KnightPathFinder.valid_moves([0,3])
+p test.new_move_positions([3,4])
+p test.considered_positions
